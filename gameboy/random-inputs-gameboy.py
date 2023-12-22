45f608743b55f70a40f34cd0d1c06b0d089afee9
Ppag <ppagdev@tuta.com>
2023-12-22 17:10:29 +0000

    implemented randomNumber() function

--- gameboy/random-inputs-gameboy.py
+++ gameboy/random-inputs-gameboy.py
@@ -1,17 +1,32 @@
+import random
+
+# all buttons:
+# L
+# R
+# UP
+# DOWN
+# LEFT
+# RIGHT
+# A
+# B
+# SELECT
+# START
+
 # button to start/stop script execution
 def toggleScript():
+    return
     # toggle script logic
 
 # pick random number
 def randomNumber():
-    totalKeys = # number of total keys
-
-    # randomly choose number between 1 and totalKeys
-
+    totalKeys = 10
+    # randomly choose number between 0 and totalKeys
+    return random.randint(0,totalKeys)
+print(randomNumber())
 # execute key press
-def pressKey(num):
-    switch (num)
-    {
-        case 0: key.press(some key),
-        # etc
-    }
+# def pressKey(num):
+#     switch (num)
+#     {
+#         case 0: key.press(some key),
+#         # etc
+#     }
