6cec206bcb89e47e5374734831bfa87ddf27c9c7
Ppag <ppagdev@tuta.com>
2023-12-22 13:33:20 +0000

    initial mock-up of random-inputs-gameboy.py

---
+++ gameboy/random-inputs-gameboy.py
@@ -0,0 +1,17 @@
+# button to start/stop script execution
+def toggleScript():
+    # toggle script logic
+
+# pick random number
+def randomNumber():
+    totalKeys = # number of total keys
+
+    # randomly choose number between 1 and totalKeys
+
+# execute key press
+def pressKey(num):
+    switch (num)
+    {
+        case 0: key.press(some key),
+        # etc
+    }
