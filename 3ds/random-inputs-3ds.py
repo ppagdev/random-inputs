fd1b3db483344c393271d38897c1e9408b0b738c
ppagdev <ppagdev@tuta.com>
2024-01-05 16:31:23 +0000

    implemented multiprocessing and cleaned up lots of code

--- 3ds/random-inputs-3ds.py
+++ 3ds/random-inputs-3ds.py
@@ -1,13 +1,14 @@
+from multiprocessing import Process, freeze_support, set_start_method
 import random
 from time import sleep
 from keycodes import *
 
 # all buttons:
 #################################
-CPAD_LEFT     =   NUMPAD_4
-CPAD_RIGHT    =   NUMPAD_6
-CPAD_UP       =   NUMPAD_8
-CPAD_DOWN     =   NUMPAD_5
+CPAD_LEFT     =   LEFT_BRACKET
+CPAD_RIGHT    =   RIGHT_BRACKET
+CPAD_UP       =   APOSTROPHE
+CPAD_DOWN     =   BACKSLASH
 #################################
 DPAD_LEFT     =   F
 DPAD_RIGHT    =   H
@@ -35,61 +36,90 @@ CONSOLE_HOME    = B
 CONSOLE_POWER   = V
 #################################
 
+myActions = []
 
+def addAction(action,frequency):
+    for i in range(frequency):
+        myActions.append(action)
 
-#####################################
-# Pokemon XY
-#####################################
-# RUN_LEFT     =   DPAD_LEFT    +   B
-# RUN_RIGHT    =   DPAD_RIGHT   +   B
-# RUN_UP       =   DPAD_UP      +   B
-# RUN_DOWN     =   DPAD_DOWN    +   B
+#################################
+addAction(CPAD_LEFT, 1)
+addAction(CPAD_RIGHT, 1)
+addAction(CPAD_UP, 1)
+addAction(CPAD_DOWN, 1)
+#################################
+addAction(DPAD_LEFT, 1)
+addAction(DPAD_RIGHT, 1)
+addAction(DPAD_UP, 1)
+addAction(DPAD_DOWN, 1)
+#################################
+addAction(CSTICK_LEFT, 1)
+addAction(CSTICK_RIGHT, 1)
+addAction(CSTICK_UP, 1)
+addAction(CSTICK_DOWN, 1)
+#################################
+addAction(CONSOLE_A, 1)
+addAction(CONSOLE_B, 1)
+addAction(CONSOLE_X, 1)
+addAction(CONSOLE_Y, 1)
+#################################
+addAction(CONSOLE_L, 1)
+addAction(CONSOLE_R, 1)
+addAction(CONSOLE_ZL, 0)
+addAction(CONSOLE_ZR, 0)
+#################################
+addAction(CONSOLE_START, 1)
+addAction(CONSOLE_SELECT, 0)
+addAction(CONSOLE_HOME, 0)
+addAction(CONSOLE_POWER, 0)
+#################################
 
+currentList = myActions
 
-
-allActions = [CPAD_LEFT, CPAD_RIGHT, CPAD_UP, CPAD_DOWN, DPAD_LEFT, DPAD_RIGHT, DPAD_UP, DPAD_DOWN, CSTICK_LEFT, CSTICK_RIGHT, CSTICK_UP, CSTICK_DOWN, CONSOLE_A, CONSOLE_B, CONSOLE_Y, CONSOLE_X, CONSOLE_L, CONSOLE_R, CONSOLE_ZL, CONSOLE_ZR, CONSOLE_START, CONSOLE_SELECT, CONSOLE_HOME, CONSOLE_POWER, 'RUN_DOWN', 'RUN_LEFT', 'RUN_RIGHT', 'RUN_UP']
+#OVERRIDES
+def overrides(action):
+    if action == CONSOLE_START:
+        if random.randint(0,10) == 0:
+            return action
+        else:
+            return 'cancel'
+    if action ==  CONSOLE_X:
+        if random.randint(0,10) == 0:
+            return action
+        else:
+            return 'cancel'
+    else:
+        return action
 
 # pick random action
 def randomAction():
-    numOfActions = len(allActions)-1
+    numOfActions = len(currentList)-1
     # randomly choose action
-    return allActions[random.randint(0,numOfActions)]
+    return currentList[random.randint(0,numOfActions)]
 
 # process action
-def processAction(action):
-    pressTime = random.uniform(0.1,1.0)
-    match action:
-        case 'RUN_DOWN':
-            HoldKey(CONSOLE_B)
-            HoldAndReleaseKey(DPAD_DOWN, pressTime)
-            ReleaseKey(CONSOLE_B)
-        case 'RUN_UP':
-            HoldKey(CONSOLE_B)
-            HoldAndReleaseKey(DPAD_UP, pressTime)
-            ReleaseKey(CONSOLE_B)
-        case 'RUN_LEFT':
-            HoldKey(CONSOLE_B)
-            HoldAndReleaseKey(DPAD_LEFT, pressTime)
-            ReleaseKey(CONSOLE_B)
-        case 'RUN_RIGHT':
-            HoldKey(CONSOLE_B)
-            HoldAndReleaseKey(DPAD_RIGHT, pressTime)
-            ReleaseKey(CONSOLE_B)
-        case default:
-            HoldAndReleaseKey(action,pressTime)
+def processAction(processIndex):
+    while True:
+        action = randomAction()
+        if overrides(action) == 'cancel':
+            print('skip')
+            continue
+        print('P' + str(processIndex) + ': ' + str(action))
+        pressTime = random.uniform(0.01,5.0)
+        HoldAndReleaseKey(action,pressTime)
 
 #countdown
-print("Starting in ")
-sleep(0.5)
-for i in range(3,0,-1):
-    print(i)
-    sleep(1)
-    
-# script loop
-sleepTime = 0.1
-print("Script is running!")
-while True:
-    processAction(randomAction())
-    sleep(sleepTime)
+def countdown():
+    print("Starting in ")
+    sleep(0.5)
+    for i in range(3,0,-1):
+        print(i)
+        sleep(1)
+    print("Script is running!")
 
-
+if __name__ == '__main__':
+    countdown()
+    freeze_support()
+    set_start_method('spawn')
+    for i in range(3):
+        Process(target=processAction, args=(i+1,)).start()
--- gameboy/random-inputs-gameboy.py
+++ gameboy/random-inputs-gameboy.py
@@ -1,86 +1,86 @@
+from multiprocessing import Process, freeze_support, set_start_method
 import random
 from time import sleep
 from keycodes import *
 
 # all buttons:
-# L       = 'q'
-# R       = 'e'
-# UP      = 'w'
-# DOWN    = 's'
-# LEFT    = 'a'
-# RIGHT   = 'd'
-# A       = 'c'
-# B       = 'x'
-# SELECT  = 'z'
-# START   = 'v'
-allKeys = ['UP','DOWN','LEFT','RIGHT','A','B','SELECT','START']
+#################################
+UP      = W
+DOWN    = S
+LEFT    = A
+RIGHT   = D
+#################################
+CONSOLE_L       = Q
+CONSOLE_R       = E
+CONSOLE_SELECT  = Z
+CONSOLE_START   = V
+#################################
+CONSOLE_A       = C
+CONSOLE_B       = X
+#################################
 
-# pick random number
-def randomNumber():
-    numOfKeys = len(allKeys)-1
-    # randomly choose number
-    return random.randint(0,numOfKeys)
+myActions = []
 
-def executeAction(key, action):
-    pressTime = random.uniform(0.1,1.0)
-    match action:
-        case 'press':
-            HoldAndReleaseKey(key, pressTime)
-        case 'hold':
-            ReleaseKey(W)
-            ReleaseKey(S)
-            ReleaseKey(A)
-            ReleaseKey(D)
-            HoldKey(key)
+def addAction(action,frequency):
+    for i in range(frequency):
+        myActions.append(action)
 
-# execute key press
-def pressKey(number):
-    key = allKeys[number]
-    actions = ['press','hold']
-    if random.randint(0,3) == 0:
-        action = actions[1]
+#################################
+addAction(UP, 1)
+addAction(DOWN, 1)
+addAction(LEFT, 1)
+addAction(RIGHT, 1)
+#################################
+addAction(CONSOLE_L, 1)
+addAction(CONSOLE_R, 1)
+addAction(CONSOLE_SELECT, 1)
+addAction(CONSOLE_START, 1)
+#################################
+addAction(CONSOLE_A, 1)
+addAction(CONSOLE_B, 1)
+#################################
+
+currentList = myActions
+
+#OVERRIDES
+def overrides(action):
+    if action == CONSOLE_START:
+        if random.randint(0,10) == 0:
+            return action
+        else:
+            return 'cancel'
     else:
-        action = actions[0]
-    print(action + '\t' + key)
-    match key:
-        case 'L':
-            executeAction(Q, 'press')
-        case 'R':
-            executeAction(E, 'press')
-        case 'UP':
-            executeAction(W, action)
-        case 'DOWN':
-            executeAction(S, action)
-        case 'LEFT':
-            executeAction(A, action)
-        case 'RIGHT':
-            executeAction(D, action)
-        case 'A':
-            executeAction(C, 'press')
-        case 'B':
-            executeAction(X, 'press')
-        case 'SELECT':
-            selectChance = random.randint(0,10)
-            if selectChance == 0:
-                executeAction(Z, 'press')
-        case 'START':
-            startChance = random.randint(0,10)
-            if startChance == 0:
-                executeAction(V, 'press')
+        return action
 
+# pick random action
+def randomAction():
+    numOfActions = len(currentList)-1
+    # randomly choose action
+    return currentList[random.randint(0,numOfActions)]
+
+# process action
+def processAction(processIndex):
+    while True:
+        action = randomAction()
+        if overrides(action) == 'cancel':
+            print('skip')
+            continue
+        print('P' + str(processIndex) + ': ' + str(action))
+        pressTime = random.uniform(0.01,5.0)
+        HoldAndReleaseKey(action,pressTime)
 
 #countdown
-print("Starting in ")
-sleep(0.5)
-for i in range(3,0,-1):
-    print(i)
-    sleep(1)
-    
-# script loop
-sleepTime = 0.1
-print("Script is running!")
-while True:
-    pressKey(randomNumber())
-    sleep(sleepTime)
+def countdown():
+    print("Starting in ")
+    sleep(0.5)
+    for i in range(3,0,-1):
+        print(i)
+        sleep(1)
+    print("Script is running!")
 
-
+if __name__ == '__main__':
+    countdown()
+    freeze_support()
+    set_start_method('spawn')
+    for i in range(3):
+        Process(target=processAction, args=(i+1,)).start()
